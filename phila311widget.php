f8cbce0316e5878bd8bc48c10bd67bf1ca3cb52a
Andrew Kennel <andrew.kennel@phila.gov>
2014-06-12 10:24:54 -0400

    Removed Map. Performance is just too slow.

--- phila311widget.php
+++ phila311widget.php
@@ -31,18 +31,6 @@ function phila311Widget_handler(){
 					}
                 }
         });
-		
-		
-//		$.getJSON(recentAPI, function (data) {
-//			$("#Phila311RecentList").empty();
-//			var i = 0;
-//			while(i < data.response.requests.length)
-//			{
-//				var newEntry = "<p><h3>" + data.response.requests[i].request.title + "</h3></p>";
-//				$("#Phila311RecentList").append(newEntry);
-//				i++;
-//			}	
-//		});
 	}
 </script>
 
@@ -72,18 +60,15 @@ float:left;
 
 <div id="Phila311Widget">
 	<span id="Phila311MainWindow">
+		<h1>Philly 311</h1>
 		<div id="Phila311LinkBlock">
 		<p><a href="http://www.publicstuff.com/pa/philadelphia-pa/report-issues">Submit New Request</a></p>
 		<p><a href="http://www.publicstuff.com/pa/philadelphia-pa/issues">Track Request</a></p>
 		<p><a href="http://www.publicstuff.com/pa/philadelphia-pa/newsfeed">News</a></p>
 		</div>
-		<div id="Phila311MapBlock">
-		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d97819.35772666815!2d-75.16781474931638!3d39.989347190470745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c6b7d8d4b54beb%3A0x89f514d88c3e58c1!2sPhiladelphia%2C+PA!5e0!3m2!1sen!2sus!4v1402504176386" width=auto height=auto frameborder="0" style="border:0"></iframe>	
-		</div>
 		<div id="Phila311RecentBlock">
+			<h4>Recent Requests</h4>
 			<ul id="Phila311RecentList">
-		
-
 			</ul>
 		</div>
 	</span>
